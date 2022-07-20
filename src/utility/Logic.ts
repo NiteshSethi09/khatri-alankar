cd8e0cd5d018fb783eb7f9d717c1a7b210c6a4ff
Nitesh Sethi <nonusethi1272@gmail.com>
2022-07-20 20:16:24 +0530

    Removed a method to make it request specific.

--- src/utility/Logic.ts
+++ src/utility/Logic.ts
@@ -1,21 +1,15 @@
-import axios from "axios";
-import { ProductItem, Products } from "./Logic.model";
+import { axiosInstance } from "./Axios";
+import { ProductItem } from "./Logic.model";
 
 export default class Logic {
   private static categories: ProductItem[] = [];
-  private static allProducts: Products[] = [];
   private constructor() {}
 
   static async getCategories(): Promise<ProductItem[]> {
     if (this.categories.length == 0) {
       try {
-        const imageData = await axios({
-          baseURL: import.meta.env.VITE_API_URL,
-          url: "/categories/get",
-          method: "get",
-        });
+        const imageData = await axiosInstance.get("/categories/get");
         const { data } = imageData.data;
-        console.log("Loading data");
 
         this.categories = data;
       } catch (e) {
@@ -24,27 +18,5 @@ export default class Logic {
     }
 
     return this.categories;
-  }
-
-  static async getAllProducts() {
-    if (this.allProducts.length == 0) {
-      try {
-        const productsData = await axios({
-          baseURL: import.meta.env.VITE_API_URL,
-          url: "/products/getWithFilters",
-          method: "post",
-          data: {},
-        });
-
-        const { data } = productsData.data;
-        console.log("Loading products data");
-        // console.log(data);
-
-        this.allProducts = data;
-      } catch (e) {
-        throw new Error("There must be some network issue.");
-      }
-    }
-    return this.allProducts;
   }
 }
